2e27e1c3c153bfaeb0b656ab5ff223f0f064fe6c
miaoyc <miaoyc1989@hotmail.com>
2023-04-04 23:52:27 +0800

    ====feat: add proxy download file====

--- download/download_test.go
+++ download/download_test.go
@@ -23,7 +23,32 @@ func Test_downloadFile(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			downloadFile(tt.args.url)
+			NormalFileDownload(tt.args.url)
+		})
+	}
+}
+
+func TestDownloadByProxy(t *testing.T) {
+	type args struct {
+		url      string
+		path     string
+		fileName string
+		proxy    string
+		timeout  int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"download-1", args{"https://github.com/miaoyc666/goArsenal/blob/master/README.md", ".", "Readme.md", "", 10}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := ProxyFileDownload(tt.args.url, tt.args.path, tt.args.fileName, tt.args.proxy, tt.args.timeout); (err != nil) != tt.wantErr {
+				t.Errorf("ProxyFileDownload() error = %v, wantErr %v", err, tt.wantErr)
+			}
 		})
 	}
 }
