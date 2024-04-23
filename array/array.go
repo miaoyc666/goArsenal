590987cca91b9bfc3a98a46bbe21703ba447d7db
miaoyc <miaoyc666@outlook.com>
2024-04-23 14:40:35 +0800

    feat(array): add MergeAndDeduplicate、SliceEqual and unit test case

--- array/array.go
+++ array/array.go
@@ -1,28 +1,59 @@
 package array
 
-import "reflect"
+import (
+	"reflect"
+)
 
 /*
 File name    : array.go
 Author       : miaoyc
 Create date  : 2023/1/11 3:01 下午
-Description  :
+Update date  : 2023/1/11 3:01 下午
+Description  : array自定义函数，使用反射的函数有一定性能损耗（非核心高性能场景可以使用）
 */
 
 // RemoveDuplicateElement 元素去重
-func RemoveDuplicateElement(values []string) []string {
-	result := make([]string, 0, len(values))
-	temp := map[string]int32{}
-	for _, item := range values {
+func RemoveDuplicateElement(input interface{}) interface{} {
+	inputVal := reflect.ValueOf(input)
+	if inputVal.Kind() != reflect.Slice {
+		panic("RemoveDuplicateElement: input is not a slice")
+	}
+
+	resultVal := reflect.MakeSlice(inputVal.Type(), 0, inputVal.Len())
+	temp := map[interface{}]struct{}{}
+	for i := 0; i < inputVal.Len(); i++ {
+		item := inputVal.Index(i).Interface()
 		if _, ok := temp[item]; !ok {
-			temp[item] = 1
-			result = append(result, item)
+			temp[item] = struct{}{}
+			resultVal = reflect.Append(resultVal, inputVal.Index(i))
 		}
 	}
-	return result
+
+	return resultVal.Interface()
 }
 
-// todo: 合并去重
+// MergeAndDeduplicate 合并两个数组并去除重复项
+func MergeAndDeduplicate(a, b interface{}) interface{} {
+	unique := make(map[interface{}]bool)
+
+	va := reflect.ValueOf(a)
+	vb := reflect.ValueOf(b)
+
+	for i := 0; i < va.Len(); i++ {
+		unique[va.Index(i).Interface()] = true
+	}
+
+	for i := 0; i < vb.Len(); i++ {
+		unique[vb.Index(i).Interface()] = true
+	}
+
+	result := reflect.MakeSlice(va.Type(), 0, len(unique))
+	for key := range unique {
+		result = reflect.Append(result, reflect.ValueOf(key))
+	}
+
+	return result.Interface()
+}
 
 // In 类python的in操作符
 func In(slice interface{}, elem interface{}) bool {
@@ -33,6 +64,42 @@ func In(slice interface{}, elem interface{}) bool {
 				return true
 			}
 		}
+	}
+	return false
+}
+
+// SliceEqual 判断两个切片是否相等
+func SliceEqual(a, b interface{}) bool {
+	countElements := func(slice interface{}) map[interface{}]int {
+		count := make(map[interface{}]int)
+		switch slice := slice.(type) {
+		case []int:
+			for _, v := range slice {
+				count[v]++
+			}
+		case []string:
+			for _, v := range slice {
+				count[v]++
+			}
+			// 添加其他类型的判断，如 []float64 等
+		}
+		return count
+	}
+
+	mapEqual := func(a, b map[interface{}]int) bool {
+		return reflect.DeepEqual(a, b)
+	}
+
+	switch a := a.(type) {
+	case []int:
+		if b, ok := b.([]int); ok {
+			return mapEqual(countElements(a), countElements(b))
+		}
+	case []string:
+		if b, ok := b.([]string); ok {
+			return mapEqual(countElements(a), countElements(b))
+		}
+		// 添加其他类型的判断，如 []float64 等
 	}
 	return false
 }
--- array/array_test.go
+++ array/array_test.go
@@ -1,7 +1,9 @@
 package array
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
@@ -20,4 +22,41 @@ func TestIn(t *testing.T) {
 	strs := []string{"0", "1", "2", "3"}
 	assert.Equal(t, true, In(strs, "1"))
 	assert.Equal(t, false, In(strs, "10"))
+}
+
+func TestRemoveDuplicateElement(t *testing.T) {
+	numbers := []int{0, 1, 2, 3, 1, 2}
+	assert.Equal(t, []int{0, 1, 2, 3}, RemoveDuplicateElement(numbers))
+	strs := []string{"0", "1", "2", "3", "1", "2"}
+	assert.Equal(t, []string{"0", "1", "2", "3"}, RemoveDuplicateElement(strs))
+}
+
+func TestMergeAndDeduplicate(t *testing.T) {
+	numbers := []int{0, 1, 2, 3, 1, 2}
+	numbers1 := []int{0, 1, 2, 3, 1, 2}
+	x := MergeAndDeduplicate(numbers, numbers1)
+	fmt.Println(x)
+	fmt.Println(reflect.TypeOf(x))
+	assert.Equal(t, true, SliceEqual([]int{0, 1, 2, 3}, MergeAndDeduplicate(numbers, numbers1)))
+	strs := []string{"0", "1", "2", "3"}
+	strs1 := []string{"3", "5", "4"}
+	y := MergeAndDeduplicate(strs, strs1)
+	fmt.Println(y)
+	fmt.Println(reflect.TypeOf(y))
+	fmt.Println([]string{"0", "1", "2", "3", "4", "5"})
+	fmt.Println(reflect.TypeOf([]string{"0", "1", "2", "3", "4", "5"}))
+	assert.Equal(t, true, SliceEqual([]string{"0", "1", "2", "3", "4", "5"}, MergeAndDeduplicate(strs, strs1)))
+}
+
+func TestSliceEqual(t *testing.T) {
+	numbers := []int{0, 1, 2, 3}
+	numbers1 := []int{0, 1, 2, 3}
+	numbers2 := []int{0, 2, 3}
+	assert.Equal(t, true, SliceEqual(numbers, numbers1))
+	assert.Equal(t, false, SliceEqual(numbers, numbers2))
+	strs := []string{"0", "1", "2", "3"}
+	strs1 := []string{"0", "1", "2", "3"}
+	strs2 := []string{"0", "1", "2"}
+	assert.Equal(t, true, SliceEqual(strs, strs1))
+	assert.Equal(t, false, SliceEqual(strs, strs2))
 }
