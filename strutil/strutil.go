3dcf16fd4a53245875f770083130c98dfad25c3f
miaoyc <miaoyc666@outlook.com>
2024-05-20 01:24:06 +0800

    feat(str): add is md5 and is uuid

--- strutil/strutil.go
+++ strutil/strutil.go
@@ -1,5 +1,10 @@
 package strutil
 
+import (
+	"regexp"
+	"strings"
+)
+
 /*
 File name    : strutil.go
 Author       : miaoyc
@@ -7,3 +12,26 @@ Create Date  : 2024/5/19 23:58
 Update Date  : 2024/5/19 23:58
 Description  :
 */
+
+var (
+	md5Pattern  = regexp.MustCompile(`^[a-fA-F0-9]{32}$`)
+	uuidPattern = regexp.MustCompile(`^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}$`)
+)
+
+func Concat(str1, str2 string, strs ...string) string {
+	var builder strings.Builder
+	builder.WriteString(str1)
+	builder.WriteString(str2)
+	for _, str := range strs {
+		builder.WriteString(str)
+	}
+	return builder.String()
+}
+
+func IsUUID(uuid string) bool {
+	return uuidPattern.MatchString(uuid)
+}
+
+func IsMD5(str string) bool {
+	return md5Pattern.MatchString(str)
+}
---
+++ strutil/strutil_test.go
@@ -0,0 +1,29 @@
+package strutil
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+/*
+File name    : strutil_test.go.go
+Create time  : 2024/5/20 01:20
+Update time  : 2024/5/20 01:20
+Description  :
+*/
+
+func TestIsUUID(t *testing.T) {
+	s := IsUUID("e397692f-1f01-4d22-bba8-60f5bca607aa")
+	assert.Equal(t, s, true)
+	s = IsUUID("e397692f-1f01-4d22")
+	assert.Equal(t, s, false)
+}
+
+func TestIsMD5(t *testing.T) {
+	s := IsMD5("35a075221d7cbbcc43842cdbbbd767f8")
+	assert.Equal(t, s, true)
+	s = IsMD5("35a075221d7cbbcc43")
+	assert.Equal(t, s, false)
+	s = IsMD5("35a075221d7cbbcc43842cdbbbd767f8x")
+	assert.Equal(t, s, false)
+}
