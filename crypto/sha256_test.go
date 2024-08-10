6c64e0296a2d008f62256578128d7b7f3052c76d
miaoyc <miaoyc666@outlook.com>
2024-08-10 23:27:12 +0800

    update

--- crypto/sha256_test.go
+++ crypto/sha256_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Test_Sha256Encrypt(t *testing.T) {
-	inputString := "Hello, SHA-256!"
+	inputString := "Hello, World!"
 	encryptedString := Sha256Encrypt(inputString)
 	fmt.Printf("原始字符串: %s\n", inputString)
 	fmt.Printf("SHA-256加密后: %s\n", encryptedString)
